b9cb502ac93b16a0b0c04695a90ea125d9953d3e
Sébastien Dumont <mailme@sebastiendumont.com>
2021-02-02 20:14:11 +0100

    📦 NEW: Custom JS

---
+++ source/javascripts/app/_cocart.js
@@ -0,0 +1,10 @@
+//= require ../lib/_jquery
+;(function () {
+	'use strict';
+
+	$(document).ready(function(){
+		// Add code label to all JSON example responses.
+		$('pre.highlight.tab-json').prepend('<div class="code-label">Example JSON API Response</div>');
+	})
+
+})();
